6356e1de75ccc73bc73ad8ab20c2f6a1d6c96a8f
Valerie <23150760+vpinspired@users.noreply.github.com>
2021-10-03 20:24:07 +0100

    Dependency issue file not found on classpath

--- familytree/src/main/java/familyTree/controller/FamilyController.java
+++ familytree/src/main/java/familyTree/controller/FamilyController.java
@@ -1,4 +1,33 @@
 package familyTree.controller;
 
+import familyTree.model.FamilyMember;
+import familyTree.repository.FamilyData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/FamilyTree")
 public class FamilyController {
+    final FamilyData familyData;
+
+    public FamilyController(FamilyData familyData) {
+        this.familyData = familyData;
+    }
+
+    @GetMapping(path = "/", produces = "application/json")
+    public List<FamilyMember> getFamily() {
+        List<FamilyMember> familyMemberList;
+        try {
+            familyMemberList = familyData.getFamilyMembers();
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "NOT FOUND (CODE 404)\n");
+        }
+        return familyMemberList;
+    }
 }
